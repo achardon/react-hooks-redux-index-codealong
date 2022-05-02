b11a9f097b90c52f81d8ae2acce8fdb94173df17
Alessandra Chardon <achardon@ucla.edu>
2022-05-02 10:48:47 -0400

    commit tree 3828afa7ded84a62b6b1e10cbbf18b6d0548c906

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+
+  const todos = useSelector(state => state.todos.entities)
+
+  return <div>
+    <h1>To Do</h1>
+    {todos.map(todo => <li>{todo}</li>)}
+  </div>;
 }
 
 export default TodoContainer;
